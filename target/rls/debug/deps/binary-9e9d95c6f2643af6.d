d2b6c520476120dc16761062448a5147ba0d6938
yobawb <qux>
2020-01-20 00:04:45 -0600

    fixed gitignore

--- target/rls/debug/deps/binary-9e9d95c6f2643af6.d
+++
@@ -1,5 +0,0 @@
-/home/yobawb/dev/rust/hello-world/target/rls/debug/deps/binary-9e9d95c6f2643af6.rmeta: 3-custom-typez/3-1-structs.rs
-
-/home/yobawb/dev/rust/hello-world/target/rls/debug/deps/binary-9e9d95c6f2643af6.d: 3-custom-typez/3-1-structs.rs
-
-3-custom-typez/3-1-structs.rs:
--- target/rls/debug/deps/binary-acb3c4591e82cceb.d
+++
@@ -1,5 +0,0 @@
-/home/yobawb/dev/rust/hello-world/target/rls/debug/deps/binary-acb3c4591e82cceb.rmeta: 3-custom-typez/3-1-structs.rs
-
-/home/yobawb/dev/rust/hello-world/target/rls/debug/deps/binary-acb3c4591e82cceb.d: 3-custom-typez/3-1-structs.rs
-
-3-custom-typez/3-1-structs.rs:
